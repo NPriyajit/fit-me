b6b0606f12bb1533f8f2fbb7437344f3db092b58
Priyajit <npriyajit1@gmail.com>
2026-07-22 12:15:06 +0530

    feat: implement PWA support with service worker registration and manifest configuration

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'fitme-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './favicon.svg',
+  './icons.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(err => console.log('Asset caching failed during sw install:', err));
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
